a75134a64ecb79bef863bd30d250b78857d7ff7c
Ashton Ferguson <doctorarferguson@gmail.com>
2023-03-04 00:12:28 -0400

    Adding a new route handler for urls.

--- express_server.js
+++ express_server.js
@@ -1,3 +1,5 @@
+//Anytime we modify the server file, we must shut down and restart the server.
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
@@ -5,6 +7,9 @@ const PORT = 8080; // default port 8080
 //This tells the Express app to use EJS as its templating engine.
 app.set("view engine", "ejs")
 
+/* This object urlDatabase will keep track of all the URLs and their shortened forms.
+This is the data we'll want to show on the URLs page. 
+Therefore, we need to pass along the urlDatabase to the template. */
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -21,6 +26,15 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+//Adding a new route handler for /urls.
+app.get("/urls", (req, res) =>{
+  const templateVars = {urls : urlDatabase };
+    /*EJS automatically knows to look inside the views directory for any template files that have the extension .ejs. This means we don't need to tell it where to find them. It also means that we do not need to include the extension of the filename when referencing it.
+  When sending variables to an EJS template, we need to send them inside an object, even if we are only sending one variable. This is so we can use the key of that variable (in the above case the key is urls) to access the data within our template. */
+  res.render("urls_index", templateVars)
+})
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
