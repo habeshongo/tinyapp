a9878704f261f71b38229c7992a721eeab1f1596
Ashton Ferguson <doctorarferguson@gmail.com>
2023-03-09 23:08:41 -0400

    [Update] - Add route to render register fields.

--- express_server.js
+++ express_server.js
@@ -3,11 +3,10 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 
 //This tells the Express app to use EJS as its templating engine.
-app.set("view engine", "ejs")
-
+app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -16,8 +15,8 @@ app.use(cookieParser());
 This is the data we'll want to show on the URLs page. 
 Therefore, we need to pass along the urlDatabase to the template. */
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
 
 app.get("/", (req, res) => {
@@ -33,24 +32,33 @@ app.get("/hello", (req, res) => {
 });
 
 //Adding a new route handler for /urls.
-app.get("/urls", (req, res) =>{
-  console.log(req.cookies['username'])
-  const templateVars = {urls : urlDatabase, username: req.cookies["username"] };
-    /*EJS automatically knows to look inside the views directory for any template files that have the extension .ejs. This means we don't need to tell it where to find them. It also means that we do not need to include the extension of the filename when referencing it.
+app.get("/urls", (req, res) => {
+  console.log(req.cookies["username"]);
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  /*EJS automatically knows to look inside the views directory for any template files that have the extension .ejs. This means we don't need to tell it where to find them. It also means that we do not need to include the extension of the filename when referencing it.
   When sending variables to an EJS template, we need to send them inside an object, even if we are only sending one variable. This is so we can use the key of that variable (in the above case the key is urls) to access the data within our template. */
-  res.render("urls_index", templateVars)
-})
+  res.render("urls_index", templateVars);
+});
 
 //This route handler will render the page with the form.
 app.get("/urls/new", (req, res) => {
-  const templateVars = {urls : urlDatabase, username: req.cookies["username"] };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_new", templateVars);
 });
 
 //Adding a new route to render the new template /urls/:id.
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"] };
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies["username"],
+  };
   res.render("urls_show", templateVars);
+});
+
+//This route render the template for the register page to login with email and password.
+app.get("/register", (req, res) => {
+  res.render("register");
 });
 
 app.post("/urls", (req, res) => {
@@ -62,36 +70,34 @@ app.post("/urls", (req, res) => {
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
-  
 });
 
 app.post("/urls/:id/delete", (req, res) => {
   // When there is a "click"
   // it deletes the key and value from the object
-  delete urlDatabase[req.params.id]
-  return res.redirect('/urls')
-})
+  delete urlDatabase[req.params.id];
+  return res.redirect("/urls");
+});
 
 app.post("/urls/:id", (req, res) => {
   urlDatabase[req.params.id] = req.body.longURL;
-  return res.redirect('/urls')
-})
+  return res.redirect("/urls");
+});
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
-  res.cookie('username', username)
-  return res.redirect('/urls')
-})
+  res.cookie("username", username);
+  return res.redirect("/urls");
+});
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('username')
-  return res.redirect('/urls')
-})
+  res.clearCookie("username");
+  return res.redirect("/urls");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
 
 // A function that returns a string of 6 random alphanumeric characters.
 function generateRandomString() {
