8e4d7a19a480576e8b7945f2ae101e0274e7f13e
Ashton Ferguson <doctorarferguson@gmail.com>
2023-03-15 23:19:30 -0300

    [Update] - Export functions to seperate file.

--- helpers.js
+++ helpers.js
@@ -1,3 +1,5 @@
+/*------------------------------Helper functions---------------------------------------*/
+
 /*This function is to find a user in the users object from its email */
 const getUserByEmail = function (email, users) {
   for (const key in users) {
@@ -9,4 +11,21 @@ const getUserByEmail = function (email, users) {
   return null;
 };
 
-module.exports = { getUserByEmail };
+// A function that returns a string of 6 random alphanumeric characters.
+const generateRandomString = function () {
+  return Math.random().toString(20).substring(2, 8);
+};
+
+/*This function returns the URLs where the userID is equal to the id of the currently logged-in user.*/
+const urlsForUser = function (id) {
+  /*This empty object was created to filter and store the websites from the urlDatabase that match the id (cookie) of the present user*/
+  const newObj = {};
+  for (const key in urlDatabase) {
+    if (id === urlDatabase[key].userID) {
+      newObj[key] = urlDatabase[key];
+    }
+  }
+  return newObj;
+};
+
+module.exports = { getUserByEmail, generateRandomString, urlsForUser };
